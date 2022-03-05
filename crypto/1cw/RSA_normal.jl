cfe54da178effa21b6ca67ebcbb5d522c114c960
Krrer-uni <wrymer2@gmail.com>
2022-03-05 19:28:17 +0100

    normal fix

--- crypto/1cw/RSA_normal.jl
+++ crypto/1cw/RSA_normal.jl
@@ -1,3 +1,15 @@
 using Statistics
 
-println(powermod(parse(BigInt, ARGS[1]),parse(Int,ARGS[3]),parse(Int,ARGS[2])))
+function RSA(x, N, e)
+    return powermod(parse(BigInt, x),parse(BigInt,e),parse(BigInt,N))
+end
+
+
+function RSA_crt(x, p, q, dp, dq, qi)
+    yp::BigInt = powermod(parse(BigInt, x),parse(BigInt,dp),parse(BigInt,p))
+    yq::BigInt = powermod(parse(BigInt, x),parse(BigInt,dq),parse(BigInt,q))
+    return yp +(yp-yq)*qi*q
+end
+
+
+println(RSA(ARGS[1], ARGS[2],ARGS[3]))
