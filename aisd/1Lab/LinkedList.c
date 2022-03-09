6cdef0e9cf7487ec0e0321499ff2242af3e87eb2
Krrer-uni <wrymer2@gmail.com>
2022-03-09 17:21:05 +0100

    working linked list

--- aisd/1Lab/LinkedList.c
+++ aisd/1Lab/LinkedList.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 struct Node{
     int value;
     struct Node* next;
 };
+
 void add_element(struct Node** List, int value){
     struct Node* new = malloc(sizeof (struct Node));
     new->next = NULL;
     new->value = value;
     
     if(*List == NULL){
-        return new;
+        *List = new;
     } else if((*List)->next ==NULL){
         (*List)->next = new;
     } else {
-        struct Node* tmp = malloc(sizeof (struct Node));
+        struct Node* tmp = *List;
         while (tmp->next->next != NULL) tmp = tmp->next;  
         tmp->next->next = new;
     }
 }
 
-int main(int argc, char* argv[]){
+int get_value_at(struct Node** List, int position){
+    struct Node* tmp = *List;
+    for(int i = 0; i < position; i++){
+        if(tmp->next == NULL)   return -1;
+        tmp = tmp->next;
+    }
+    return tmp->value;
+}
 
+int main(int argc, char* argv[]){
+    struct Node* LinkedList = NULL;
+    for(int i = 0 ; i < 1000; i ++){
+        add_element(&LinkedList, i);
+    }
+    for(int i = 0; i < 1000; i++){
+        printf("%d\n",get_value_at(&LinkedList, i));
+    }
 }
