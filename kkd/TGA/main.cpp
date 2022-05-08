4528839eef83f537242aac3180c82e90a67aff43
Krrer-uni <wrymer2@gmail.com>
2022-05-08 13:05:06 +0200

    lab 3 init

--- kkd/TGA/main.cpp
+++ kkd/TGA/main.cpp
@@ -11,7 +11,36 @@ struct RGB {
 #include <vector>
 #include <cmath>
 
-void entropy(std::vector<std::vector<RGB>> pixels, int width, int height) {
+
+std::vector<std::vector<RGB>> pack(std::vector<std::vector<RGB>> pixels, int width, int height) {
+    RGB black;
+    black.red = 0;
+    black.green = 0;
+    black.blue = 0;
+    std::vector<RGB> black_row(width + 2);
+    for (int i = 0; i < width + 2; i++) black_row[i] = black;
+    for (int i = 0; i < width; i++) {
+        pixels[i].insert(pixels[i].begin(), black);
+        pixels[i].push_back(black);
+    }
+    pixels.insert(pixels.begin(), black_row);
+    pixels.push_back(black_row);
+    return pixels;
+}
+
+std::vector<std::vector<RGB>> unpack(std::vector<std::vector<RGB>> pixels, int width, int height) {
+
+    pixels.pop_back();
+    pixels.erase(pixels.begin());
+    for (int i = 0; i < width; i++) {
+        pixels[i].pop_back();
+        pixels[i].erase(pixels[i].begin());
+    }
+
+    return pixels;
+}
+
+void entropy(std::vector<std::vector<RGB>> pixels, int width, int height, std::vector<std::vector<double>> &scores) {
     int char_counters_red[256]{0};
     int char_counters_green[256]{0};
     int char_counters_blue[256]{0};
@@ -37,18 +66,26 @@ void entropy(std::vector<std::vector<RGB>> pixels, int width, int height) {
                          (log2((double) (char_counters_blue[i] + char_counters_red[i] + char_counters_green[i])) -
                           log_count_all);
     }
-    for(int i = 0; i < 3; i++){
-        result[i] /= -(double)(width * height);
-        std::cout << result[i] << std::endl;
+    for (int i = 0; i < 3; i++) {
+        result[i] /= -(double) (width * height);
+//        std::cout << result[i] << std::endl;
     }
-    result[3] /= -(double)(width * height * 3);
-    std::cout << result[3];
+    result[3] /= -(double) (width * height * 3);
+    std::cout << "whole file entropy " << result[3] << std::endl;
+    std::cout << "red color entropy " << result[0] << std::endl;
+    std::cout << "green color entropy " << result[1] << std::endl;
+    std::cout << "blue color entropy " << result[2] << std::endl;
+    std::vector<double> local_scores;
+    for(int i = 0; i < 4; i++){
+        local_scores.push_back(result[i]);
+    }
+    scores.push_back(local_scores);
 }
 
 int main() {
     //read header
     const char *filename = "example0.tga";
-    FILE *f = std::fopen("../test/example0.tga", "rb");
+    FILE *f = std::fopen("../test/example1.tga", "rb");
     tga::StdioFileInterface file(f);
     tga::Decoder decoder(&file);
     tga::Header header;
@@ -77,11 +114,152 @@ int main() {
 //    for(auto pixel : buffer){
 //        std::cout << (int)pixel << std::endl;
 //    }
+    std::vector<std::vector<double>> scores;
     std::vector<std::vector<RGB>> pixels(header.width, std::vector<RGB>(header.height));
     for (int i = 0; i < header.width * header.height; i++) {
         (pixels[i % header.width][i / header.width]).red = image.pixels[i * 4];
         pixels[i % header.width][i / header.width].green = image.pixels[i * 4 + 1];
         pixels[i % header.width][i / header.width].blue = image.pixels[i * 4 + 2];
     }
-    entropy(pixels, header.width, header.height);
+    std::cout << "unmodified file" << std::endl;
+    entropy(pixels, header.width, header.height, scores);
+    auto packed = pack(pixels, header.width, header.height);
+    std::vector<std::vector<RGB>> pred_1(header.width, std::vector<RGB>(header.height));
+
+    std::cout << "pred_1" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+            pred_1[i - 1][j - 1].red = packed[i][j].red - packed[i - 1][j].red;
+            pred_1[i - 1][j - 1].green = packed[i][j].green - packed[i - 1][j].green;
+            pred_1[i - 1][j - 1].blue = packed[i][j].blue - packed[i - 1][j].blue;
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    std::cout << "pred_2" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+            pred_1[i - 1][j - 1].red = packed[i][j].red - packed[i][j - 1].red;
+            pred_1[i - 1][j - 1].green = packed[i][j].green - packed[i][j - 1].green;
+            pred_1[i - 1][j - 1].blue = packed[i][j].blue - packed[i][j - 1].blue;
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    std::cout << "pred_3" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+            pred_1[i - 1][j - 1].red = packed[i][j].red - packed[i - 1][j - 1].red;
+            pred_1[i - 1][j - 1].green = packed[i][j].green - packed[i - 1][j - 1].green;
+            pred_1[i - 1][j - 1].blue = packed[i][j].blue - packed[i - 1][j - 1].blue;
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    std::cout << "pred_4" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+            pred_1[i - 1][j - 1].red =
+                    packed[i][j].red - (packed[i][j - 1].red + packed[i - 1][j].red - packed[i - 1][j - 1].red);
+            pred_1[i - 1][j - 1].green =
+                    packed[i][j].green - (packed[i][j - 1].green + packed[i - 1][j].green - packed[i - 1][j - 1].green);
+            pred_1[i - 1][j - 1].blue =
+                    packed[i][j].blue - (packed[i][j - 1].blue + packed[i - 1][j].blue - packed[i - 1][j - 1].blue);
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    std::cout << "pred_5" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+            pred_1[i - 1][j - 1].red =
+                    packed[i][j].red - (packed[i][j - 1].red + (packed[i - 1][j].red - packed[i - 1][j - 1].red) / 2);
+            pred_1[i - 1][j - 1].green = packed[i][j].green - (packed[i][j - 1].green +
+                                                               (packed[i - 1][j].green - packed[i - 1][j - 1].green) /
+                                                               2);
+            pred_1[i - 1][j - 1].blue = packed[i][j].blue - (packed[i][j - 1].blue +
+                                                             (packed[i - 1][j].blue - packed[i - 1][j - 1].blue) / 2);
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    std::cout << "pred_6" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+            pred_1[i - 1][j - 1].red =
+                    packed[i][j].red - (packed[i - 1][j].red + (packed[i][j - 1].red - packed[i - 1][j - 1].red) / 2);
+            pred_1[i - 1][j - 1].green = packed[i][j].green - (packed[i - 1][j].green +
+                                                               (packed[i][j - 1].green - packed[i - 1][j - 1].green) /
+                                                               2);
+            pred_1[i - 1][j - 1].blue = packed[i][j].blue - (packed[i - 1][j].blue +
+                                                             (packed[i][j - 1].blue - packed[i - 1][j - 1].blue) / 2);
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    std::cout << "pred_7" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+            pred_1[i - 1][j - 1].red = packed[i][j].red - (packed[i][j - 1].red + packed[i - 1][j].red) / 2;
+            pred_1[i - 1][j - 1].green = packed[i][j].green - (packed[i][j - 1].green + packed[i - 1][j].green) / 2;
+            pred_1[i - 1][j - 1].blue = packed[i][j].blue - (packed[i][j - 1].blue + packed[i - 1][j].blue) / 2;
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    std::cout << "new standard" << std::endl;
+    for (int i = 1; i < header.width + 1; i++) {
+        for (int j = 1; j < header.height + 1; j++) {
+
+            uint8_t x_hat;
+            if (packed[i - 1][j - 1].red >=
+                std::max(packed[i][j - 1].red, packed[i - 1][j].red)) {
+                x_hat = std::max(packed[i][j - 1].red, packed[i - 1][j].red);
+            } else if (packed[i - 1][j - 1].red >=
+                       std::min(packed[i][j - 1].red, packed[i - 1][j].red)) {
+                x_hat = std::min(packed[i][j - 1].red, packed[i - 1][j].red);
+            } else {
+                x_hat = packed[i][j - 1].red + packed[i - 1][j].red - packed[i - 1][j - 1].red;
+            }
+            pred_1[i - 1][j - 1].red = packed[i][j].red - x_hat;
+
+            if (packed[i - 1][j - 1].green >=
+                std::max(packed[i][j - 1].green, packed[i - 1][j].green)) {
+                x_hat = std::max(packed[i][j - 1].green, packed[i - 1][j].green);
+            } else if (packed[i - 1][j - 1].green >=
+                       std::min(packed[i][j - 1].green, packed[i - 1][j].green)) {
+                x_hat = std::min(packed[i][j - 1].green, packed[i - 1][j].green);
+            } else {
+                x_hat = packed[i][j - 1].green + packed[i - 1][j].green - packed[i - 1][j - 1].green;
+            }
+            pred_1[i - 1][j - 1].green = packed[i][j].green - x_hat;
+
+            if (packed[i - 1][j - 1].blue >=
+                std::max(packed[i][j - 1].blue, packed[i - 1][j].blue)) {
+                x_hat = std::max(packed[i][j - 1].blue, packed[i - 1][j].blue);
+            } else if (packed[i - 1][j - 1].blue >=
+                       std::min(packed[i][j - 1].blue, packed[i - 1][j].blue)) {
+                x_hat = std::min(packed[i][j - 1].blue, packed[i - 1][j].blue);
+            } else {
+                x_hat = packed[i][j - 1].blue + packed[i - 1][j].blue - packed[i - 1][j - 1].blue;
+            }
+            pred_1[i - 1][j - 1].blue = packed[i][j].blue - x_hat;
+        }
+    }
+    entropy(pred_1, header.width, header.height, scores);
+
+    int best_scores[4]{10,10,10,10};
+    for(int i = 0; i < 4; i++){
+        double min = 10.0;
+        for(int j = 0; j < 8; j++){
+            if(scores[j][i] < min){
+                best_scores[i] = j;
+                min =  scores[j][i];
+            }
+        }
+    }
+
+    for(int i = 0; i < 4; i++){
+        std::cout << best_scores[i] << std::endl;
+    }
 }
